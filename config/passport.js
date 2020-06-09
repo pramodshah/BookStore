860f5565cc46dfa8ceb234437a6a6a7d52bad60e
Pramod shah <pramodshah1444@gmail.com>
2020-06-10 01:36:08 +0530

    Updated

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
+
 module.exports = function(passport){ 
+    
     passport.use(
         new localStrategy({usernameField:'email'},(email,password,done)=>{
             // match user
